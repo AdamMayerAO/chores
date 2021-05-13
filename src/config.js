b85c450369842c0ac272371963c4ab6933c252d9
Adam Mayer <r.adammayer@gmail.com>
2021-05-12 21:35:57 -0400

    favicon

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default{
-    API_ENDPOINT: 'http://localhost:8000' || `https://choresserver.herokuapp.com/`
+    API_ENDPOINT: `https://choresserver.herokuapp.com/` ||'http://localhost:8000'
 }
